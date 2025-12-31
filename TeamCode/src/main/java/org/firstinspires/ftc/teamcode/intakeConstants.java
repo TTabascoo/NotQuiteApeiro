672e7a2e3987fef0bde4c4511aaa960e1f778250
Ewan <ewanlee1215@gmail.com>
2025-12-31 10:05:37 -0500

    intake constants for direction and power and locker subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intakeConstants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class intakeConstants {
+    public static double intakePower;
+    public static double intakedirection = 1;
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/locker.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import static dev.nextftc.bindings.Bindings.button;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import dev.nextftc.bindings.Button;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.LambdaCommand;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.positionable.SetPosition;
+import kotlin.jvm.internal.Lambda;
+
+public class locker implements Subsystem {
+    public static final locker INSTANCE = new locker();
+    private ServoEx locker = new ServoEx("locker");
+
+    public Command open() {
+        return new LambdaCommand()
+                .setStart(() -> {
+                    new SetPosition(locker, 1.0);
+        }).setRequirements(this);
+    }
+    public Command close() {
+        return new LambdaCommand()
+                .setStart(() -> {
+                    new SetPosition(locker, 0);
+                }).setRequirements(this);
+    }
+    public void buttonMap(Gamepad gamepad) {
+        Button open = button(() -> gamepad.square)
+                .toggleOnBecomesTrue()
+                .whenBecomesTrue(() -> open())
+                .whenBecomesFalse(() -> close());
+
+    }
+
+
+}
