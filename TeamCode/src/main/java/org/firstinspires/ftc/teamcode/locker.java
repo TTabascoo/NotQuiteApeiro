e2e05ff7f5e7a9e97683b86ec7ccc0c44c328032
Ewan <ewanlee1215@gmail.com>
2026-01-10 20:54:53 -0500

    change values and remove unnecessary code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/locker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/locker.java
@@ -18,26 +18,8 @@ public class locker implements Subsystem {
     public static final locker INSTANCE = new locker();
     private final ServoEx locker = new ServoEx("locker");
 
-    @Override
-    public void periodic() {
-//        ActiveOpMode.telemetry().addData("locker position", locker.getPosition());
-//        ActiveOpMode.telemetry().update();
-    }
-
-//    public LambdaCommand open() {
-//        return new LambdaCommand()
-//                .setStart(() -> {
-//                    new SetPosition(locker, 1.0);
-//        }).setRequirements(this);
-//    }
-//    public LambdaCommand close() {
-//        return new LambdaCommand()
-//                .setStart(() -> {
-//                    new SetPosition(locker, 0);
-//                }).setRequirements(this);
-//    }
     public Command open() {
-        return new InstantCommand(new SetPosition(locker, 1.0)).requires(this);
+        return new InstantCommand(new SetPosition(locker, 0.5)).requires(this);
     }
     public Command close() {
         return new InstantCommand(new SetPosition(locker, 0)).requires(this);
